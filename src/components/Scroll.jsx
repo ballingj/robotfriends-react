a0bea528bc713f4dbf5b2956d4e585c65b76b762
ballingj <jeffrey.ballinger@gmail.com>
2025-03-13 19:56:36 -0500

    fixed shrinking width in css

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
+    <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
       {props.children}
     </div>
   )
