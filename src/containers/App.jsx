3560533a6080df9d6554256d8dacf45fc9fce72b
ballingj <jeffrey.ballinger@gmail.com>
2025-03-15 01:59:44 -0500

    useState and useEffects

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -29,7 +29,6 @@ function App () {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => { setRobots(users) });
-    // console.log(count)
   }, [])
   */}
 
