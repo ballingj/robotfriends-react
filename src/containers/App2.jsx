9aa30d525e022629ca4dd6d4fb2d40f233246ccd
ballingj <jeffrey.ballinger@gmail.com>
2025-03-15 01:58:15 -0500

    useState and useEffects

--- src/containers/App2.jsx
+++ src/containers/App2.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import reactLogo from '/logo/react.svg'
+import viteLogo from '/logo/vite.svg'
 import './App.css'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
@@ -6,47 +8,46 @@ import Scroll from '../components/Scroll'
 // import { robots } from './robots'
 
 
-function App () {
-  const [ robots, setRobots ] = useState([]);
-  const [ searchfield, setSearchfield ] = useState('');
-  const url = 'https://jsonplaceholder.typicode.com/users'
+function App() {
+
+  const { robots, setRobots } = useState([]);
+  const { searchfield, setSearchfield } = useState('');
+
 
   useEffect(() => {
-    async function fetchUsers() {  
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setRobots(data);
-      } catch (e) {
-          console.log('Error: ', e)
-      }
+    const url = 'https://jsonplaceholder.typicode.com/users'
+
+    const data = async function fetchUsers() {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      return data;
     }
     fetchUsers();
+    setRobots(data);
   }, [])
-
-  {/* // simple promise
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => { setRobots(users) });
-    // console.log(count)
-  }, [])
-  */}
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
   }
-  
+
+  // render() {
+  //   const { robots, searchfield } = this.state;
   const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase())
   })
-  
-  // console.log('robots', robots, 'searchfield', searchfield)
+
   return !robots.length ?
     <h1>Loading...</h1> :
     (
       <>
         <div className="title">
+          {/* <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a> */}
           <h1 className="robofriend">RobotFriends</h1>
           <SearchBox searchChange={onSearchChange} />
         </div>
